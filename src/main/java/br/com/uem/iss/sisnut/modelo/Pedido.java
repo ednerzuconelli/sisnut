cf0bef71b89fada3e2d8c46d8169d7c39e832a80
ednerzuconelli <ednerzuconelli@gmail.com>
2016-01-29 14:10:20 -0200

    pedido

--- src/main/java/br/com/uem/iss/sisnut/modelo/Pedido.java
+++ src/main/java/br/com/uem/iss/sisnut/modelo/Pedido.java
@@ -2,12 +2,19 @@ package br.com.uem.iss.sisnut.modelo;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.joda.time.DateTime;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -22,7 +29,35 @@ public class Pedido {
 	@Column(name="pedido_id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
 	@DateTimeFormat(iso=ISO.DATE)
 	@Column(name="data_pedido", nullable=false)
 	private DateTime  dataPedido;
+	
+	@Column(name="quantidade")
+	private Double quantidade;
+	
+	@Column(name="valor_unitario")
+	private Double valorUnitario;
+	
+	@Column(name="enviou")
+	private Integer enviou;
+	
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="produto_id", updatable=true, insertable=true, nullable=true)
+	@Fetch(FetchMode.SELECT)
+	@Cascade(CascadeType.ALL)
+	private Produto produto;
+	
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="paciente_id", updatable=true, insertable=true, nullable=true)
+	@Fetch(FetchMode.SELECT)
+	@Cascade(CascadeType.ALL)
+	private Paciente paciente;
+
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="fornecedor_id", updatable=true, insertable=true, nullable=true)
+	@Fetch(FetchMode.SELECT)
+	@Cascade(CascadeType.ALL)
+	private Fornecedor fornecedor;
 }
