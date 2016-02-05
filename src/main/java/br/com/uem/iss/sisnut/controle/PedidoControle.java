b436e97d5d6a687b4a9d5de3c6938e58c3b6ee6c
ednerzuconelli <ednerzuconelli@gmail.com>
2016-02-05 18:33:11 -0200

    correçoes e melhorias

--- src/main/java/br/com/uem/iss/sisnut/controle/PedidoControle.java
+++ src/main/java/br/com/uem/iss/sisnut/controle/PedidoControle.java
@@ -1,5 +1,8 @@
 package br.com.uem.iss.sisnut.controle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.binding.message.MessageBuilder;
 import org.springframework.binding.message.MessageContext;
@@ -8,7 +11,9 @@ import org.springframework.webflow.action.EventFactorySupport;
 import org.springframework.webflow.execution.Event;
 
 import br.com.uem.iss.sisnut.modelo.Pedido;
+import br.com.uem.iss.sisnut.modelo.Produto;
 import br.com.uem.iss.sisnut.servico.PedidoServico;
+import br.com.uem.iss.sisnut.servico.ProdutoServico;
 import br.com.uem.iss.sisnut.view.PedidoBean;
 
 @Component("pedidoControle")
@@ -17,9 +22,26 @@ public class PedidoControle {
 	@Autowired
 	private PedidoServico pedidoservico;
 	
+	@Autowired
+	private ProdutoServico produtoservico;
+	
 	public PedidoBean newPedidoBean(){
     	Pedido pedido = new Pedido();
     	return new PedidoBean(pedido, new EventFactorySupport().success(this));
+    }
+	
+	public List<Produto> completeTheme(String query) {
+        List<Produto> produtos = produtoservico.findAll();
+        List<Produto> filteredProdutos = new ArrayList<Produto>();
+         
+        for (int i = 0; i < produtos.size(); i++) {
+            Produto skin = produtos.get(i);
+            if(skin.getProduto().toLowerCase().startsWith(query)) {
+                filteredProdutos.add(skin);
+            }
+        }
+         
+        return filteredProdutos;
     }
 	
 	
