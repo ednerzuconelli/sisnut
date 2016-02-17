6c4353de31e355a163b83e56eb8aa9c10de4bf33
ednerzuconelli <ednerzuconelli@gmail.com>
2016-02-17 20:18:07 -0200

    teste e email de pedido

--- src/main/java/testes/testePedido.java
+++ src/main/java/testes/testePedido.java
@@ -1,29 +1,32 @@
 package testes;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.binding.message.MessageContext;
-import org.springframework.webflow.execution.Event;
-
-import br.com.uem.iss.sisnut.controle.PacienteControle;
-import br.com.uem.iss.sisnut.modelo.Paciente;
+import br.com.uem.iss.sisnut.modelo.Pedido;
 import br.com.uem.iss.sisnut.servico.PacienteServico;
-import br.com.uem.iss.sisnut.view.PacienteBean;
+import br.com.uem.iss.sisnut.servico.PedidoServico;
+import br.com.uem.iss.sisnut.servico.ProdutoServico;
 
 public class testePedido {
 
 	@Autowired
 	private PacienteServico pacienteservico;
 	
+	@Autowired
+	private PedidoServico pedidoservico;
+	
+	@Autowired
+	private ProdutoServico produtoservico;
+	
 	@Test
 	public void deveriaCriarLogQuandoInserePedido() {
 		
-		Event success = null;
-		PacienteBean paciente = new PacienteBean(success);
-		paciente.setNome("teste");
-		//pacienteservico.save(paciente);
+		Pedido pedido = new Pedido();
+		pedido.setPaciente(pacienteservico.findPacienteById(1));
+		pedido.setProduto(produtoservico.getById(1));
+		pedido.setQuantidade(1.0);
+		pedido.setValorUnitario(10.0);
+		pedidoservico.save(pedido);
 		
 	}
 
